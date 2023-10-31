2e7cbac5de5fcd92eeb373dcf9d52797086784c0
Kiyotaka <114660113+Kiyotaka-aynokodzi@users.noreply.github.com>
2023-10-31 06:26:10 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -34,6 +34,7 @@ public class Requester
 
     public void Request()
     {
+        int data = 1;
         bool check = true;
         double result = 0;
         while (check == true)
@@ -42,62 +43,66 @@ public class Requester
             Console.WriteLine("Введите первое число");
             double number = Convert.ToInt64(Console.ReadLine());
             Console.WriteLine("Выберите метод");
-            string method = Console.ReadLine();
+            string methods = Console.ReadLine();
             Console.WriteLine("Введите второе число");
             double moreNumber = Convert.ToInt64(Console.ReadLine());
 
-            switch (method)
+            if (methods == "/")
             {
-                case "+":
-                    result = cal.Addition(number, moreNumber);
-                    Console.WriteLine(result);
-                    break;
-
-                case "*":
-                    result = cal.Multiply(number, moreNumber);
-                    Console.WriteLine(result);
-                    break;
-
-                case "-":
-                    result = cal.Subtraction(number, moreNumber);
-                    Console.WriteLine(result);
-                    break;
-
-                case "/":
-                    result = cal.Division(number, moreNumber);
-                    if (moreNumber != 0)
-                    {
-                        Console.WriteLine(result);
-                    }
-                    else
-                    {
-                        Console.WriteLine("на 0 делить нельзя");
-                    }
-                    break;
+                result = cal.Division(number, moreNumber);
+                Console.WriteLine(result);
+            }
+            else if (methods == "+")
+            {
+                result = cal.Addition(number, moreNumber);
+                Console.WriteLine(result);
+            }
+            else if (methods == "-")
+            {
+                result = cal.Subtraction(number, moreNumber);
+                Console.WriteLine(result);
+            }
+            else if (methods == "*")
+            {
+                result = cal.Multiply(number, moreNumber);
+                Console.WriteLine(result);
             }
             number = result;
 
             Console.WriteLine("Продолжить?");
             string question = Console.ReadLine();
-            if (question == "Yes") { }
+            if (question == "Yes")
+            {
+                data = 0;
+                if(data < 1)
+                {
+                    Console.WriteLine(result);
+                    data++;
+                }
+            }
             else if (question == "No")
             {
                 Console.WriteLine("Очистить калькулятор?");
                 string moreQuestion = Console.ReadLine();
                 if (moreQuestion == "Yes")
                 {
-                    Console.WriteLine("");
+                    Console.WriteLine("пока");
                     Console.ReadKey();
                     check = false;
+                } else if(moreQuestion == "No"){
+                    check = false;
+                    data = 0;
+                    if(data <= 1) {
+                        Console.WriteLine(result);
+                    }
+                    Console.WriteLine("пока");
                 }
             }
             else
             {
                 Console.WriteLine("Вы не ответили на вопрос");
             }
-
         }
-
     }
 }
 
@@ -105,8 +110,12 @@ public class Program
 {
     static void Main()
     {
-        Calculator cal = new Calculator();
-        Requester Request = new Requester(cal);
-        Request.Request();
+        bool proverka = true;
+        while (proverka == true)
+        {
+            Calculator cal = new Calculator();
+            Requester Request = new Requester(cal);
+            Request.Request();
+        }
     }
 }
